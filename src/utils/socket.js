257203515b89027be6bb1a79360c908de97647f6
HyukE <ming2ki2@gmail.com>
2023-03-15 15:47:44 +0900

    refactor: socketio셋업수정

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,27 +3,29 @@ const http = require("http");
 const socketIO = require("socket.io");
 
 const server = http.createServer(app);
-
 server.listen(process.env.PORT || 4000, () => {});
 
 const io = socketIO(server, {
   cors: {
     origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
   },
 });
 
-const lobbyNamespace = io.of("/lobby");
-lobbyNamespace.on("connection", (socket) => {
-  const currentUser = [];
-  const { user, userProfile } = socket.handshake.query;
-  currentUser.push({ user: user, userProfile: userProfile });
+const lobby = io.of("/");
+const battles = io.of(/^\/battles\/.+$/);
 
-  socket.on("updateChat", ({ user, content }) => {
-    socket.broadcast.emit("chatUpdated", { user, content });
+lobby.on("connection", (socket) => {
+  const currentUser = [];
+  const { user } = socket.handshake.query;
+  currentUser.push({ user });
+
+  socket.on("send-chat", ({ user, chat }) => {
+    io.broadcast.emit("broadcast-chat", user, chat);
   });
 
-  socket.on("updateCurrentUser", () => {
-    socket.broadcast.emit("CurrentUserUpdated", currentUser);
+  socket.on("send-user", () => {
+    io.broadcast.emit("update-user", currentUser);
   });
 
   socket.on("disconnect", () => {
@@ -31,19 +33,19 @@ lobbyNamespace.on("connection", (socket) => {
     const index = currentUser.findIndex(
       (userObj) => userObj.user === disconnectedUser,
     );
+
     if (index !== -1) {
       currentUser.splice(index, 1);
-      io.emit("CurrentUserUpdated", currentUser);
+      io.emit("update-user", currentUser);
     }
   });
 });
 
-const battlesNamespace = io.of(/^\/battles\/(.+)$/);
-battlesNamespace.on("connection", (socket) => {
+battles.on("connection", (socket) => {
   const roomId = socket.nsp.name.split("/")[2];
 
-  socket.on("updateKey", ({ key }) => {
-    io.to(roomId).emit("keyUpdated", key);
+  socket.on("send-data", ({ key, score }) => {
+    socket.to(roomId).emit("receive-data", key, score);
   });
 
   socket.on("disconnect", () => {});
