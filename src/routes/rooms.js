98b6b6f615bf3bc4040632d6917ef2653b3bb4dc
oyobbeb <oyobbeb@gmail.com>
2023-03-21 22:27:33 +0900

    feat: verifyToken 미들웨어 구현 및 eslint rule 추가

--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const roomController = require("./controllers/room.Controller");
+const verifyToken = require("./middlewares/verifyToken");
 
 router.get("/", roomController.getRooms);
 
-router.get("/new", roomController.getSongs);
-router.post("/new", roomController.makeRoom);
+router.get("/new", verifyToken, roomController.getSongs);
+router.post("/new", verifyToken, roomController.makeRoom);
 
-router.get("/:roomId", roomController.getBattleData);
+router.get("/:roomId", verifyToken, roomController.getBattleData);
 
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -5,11 +5,4 @@ const userController = require("./controllers/user.Controller");
 router.post("/login", userController.loginUser);
 router.post("/logout", userController.logoutUser);
 
-router.post("/", async (req, res, next) => {
-  try {
-  } catch (err) {
-    next(err);
-  }
-});
-
 module.exports = router;
