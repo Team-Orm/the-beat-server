390332c063055aca6ea474971c316594eeed3470
oyobbeb <oyobbeb@gmail.com>
2023-06-16 01:54:29 +0900

    fix: script 수정 및 소켓 서버 통합

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,3 @@
-require("dotenv").config();
-const app = require("../app");
-const http = require("http");
-const socketIO = require("socket.io");
 const BattleRoom = require("../models/BattleRoom");
 const {
   UPDATE_ROOMS,
@@ -33,178 +29,167 @@ const {
   RECEIVE_RESULTS,
 } = require("../constants/eventName");
 
-const server = http.createServer(app);
-
-server.listen(process.env.PORT || 4000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 4000}`);
-});
-
 const MongoClient = require("mongodb").MongoClient;
 const uri = process.env.SECRET_MONGODB_ID;
 
-const io = socketIO(server, {
-  cors: {
-    origin: process.env.CORS_ORIGIN,
-    methods: ["GET", "POST"],
-  },
-});
+module.exports = function setupSocketServer(io) {
+  const battles = io.of("/battles/");
+  const results = io.of("/results/");
+  const battleRooms = {};
+  const usersInRoom = {};
+  let usersInLobby = {};
 
-const battles = io.of("/battles/");
-const results = io.of("/results/");
-const battleRooms = {};
-const usersInRoom = {};
-let usersInLobby = {};
+  (async () => {
+    const client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-(async () => {
-  const client = await MongoClient.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+    const db = client.db(BEAT);
+    const collection = db.collection("battlerooms");
 
-  const db = client.db(BEAT);
-  const collection = db.collection("battlerooms");
+    const changeStream = collection.watch();
 
-  const changeStream = collection.watch();
+    changeStream.on("change", async () => {
+      try {
+        const rooms = await BattleRoom.find().populate("song");
 
-  changeStream.on("change", async () => {
-    try {
-      const rooms = await BattleRoom.find().populate("song");
+        io.of("/").emit(UPDATE_ROOMS, rooms);
+      } catch (err) {
+        next(err);
+      }
+    });
+  })();
 
-      io.of("/").emit(UPDATE_ROOMS, rooms);
-    } catch (err) {
-      next(err);
-    }
-  });
-})();
+  io.on("connection", (socket) => {
+    const { displayName, photoURL, uid } = socket.handshake.query;
+    socket.join(uid);
 
-io.on("connection", (socket) => {
-  const { displayName, photoURL, uid } = socket.handshake.query;
-  socket.join(uid);
-
-  if (!usersInLobby[uid]) {
-    usersInLobby = {
-      ...usersInLobby,
-      [uid]: { displayName, photoURL },
-    };
-  }
-
-  const convertedRooms = Object.keys(battleRooms).reduce((acc, key) => {
-    acc[key] = { users: Array.from(battleRooms[key].users) };
-    return acc;
-  }, {});
-
-  io.emit(LOBBY_ROOMS, convertedRooms);
-
-  socket.on(RECEIVE_LOBBY_USERS, () => {
-    io.emit(LOBBY_ROOMS, convertedRooms);
-    io.emit(UPDATE_USER, Object.values(usersInLobby));
-  });
-
-  socket.on(SEND_CHAT, ({ user, chat }) => {
-    io.emit(BROADCAST_CHAT, user, chat);
-  });
-
-  socket.on("reconnect", () => {
     if (!usersInLobby[uid]) {
       usersInLobby = {
         ...usersInLobby,
         [uid]: { displayName, photoURL },
       };
-      io.emit(UPDATE_USER, Object.values(usersInLobby));
     }
-  });
 
-  socket.on("disconnect", () => {
-    const { uid } = socket.handshake.query;
-    delete usersInLobby[uid];
+    const convertedRooms = Object.keys(battleRooms).reduce((acc, key) => {
+      acc[key] = { users: Array.from(battleRooms[key].users) };
+      return acc;
+    }, {});
 
-    io.emit(UPDATE_USER, Object.values(usersInLobby));
-  });
-});
-
-battles.on("connection", (socket) => {
-  const { photoURL, displayName, uid, roomId } = socket.handshake.query;
-  socket.join(roomId);
-
-  const user = { photoURL, displayName, uid };
-
-  const room = battleRooms[roomId] || { users: new Set() };
-  room.users.add(uid);
-  battleRooms[roomId] = room;
-
-  if (room.users.size > 2) {
-    socket.emit(ROOM_FULL);
-    socket.leave(roomId);
-    return;
-  }
-
-  const convertedRooms = Object.keys(battleRooms).reduce((acc, key) => {
-    acc[key] = { users: Array.from(battleRooms[key].users) };
-    return acc;
-  }, {});
-
-  if (!usersInRoom[roomId]) {
-    usersInRoom[roomId] = [];
-  }
-  usersInRoom[roomId].push(user);
-
-  socket.on(SEND_CONNECT, () => {
     io.emit(LOBBY_ROOMS, convertedRooms);
+
+    socket.on(RECEIVE_LOBBY_USERS, () => {
+      io.emit(LOBBY_ROOMS, convertedRooms);
+      io.emit(UPDATE_USER, Object.values(usersInLobby));
+    });
+
+    socket.on(SEND_CHAT, ({ user, chat }) => {
+      io.emit(BROADCAST_CHAT, user, chat);
+    });
+
+    socket.on("reconnect", () => {
+      if (!usersInLobby[uid]) {
+        usersInLobby = {
+          ...usersInLobby,
+          [uid]: { displayName, photoURL },
+        };
+        io.emit(UPDATE_USER, Object.values(usersInLobby));
+      }
+    });
+
+    socket.on("disconnect", () => {
+      const { uid } = socket.handshake.query;
+      delete usersInLobby[uid];
+
+      io.emit(UPDATE_USER, Object.values(usersInLobby));
+    });
   });
 
-  socket.on(SEND_USER, () => {
-    socket.to(roomId).emit(RECEIVE_USER, usersInRoom[roomId]);
-  });
+  battles.on("connection", (socket) => {
+    const { photoURL, displayName, uid, roomId } = socket.handshake.query;
+    socket.join(roomId);
 
-  socket.on(DELETE_ROOM, () => {
-    socket.to(roomId).emit(RECEIVE_DELETE);
-  });
+    const user = { photoURL, displayName, uid };
 
-  socket.on(OPPONENT_KEY_PRESS, (key) => {
-    socket.to(roomId).emit(RECEIVE_OPPONENT_KEY_PRESS, key);
-  });
-
-  socket.on(OPPONENT_KEY_RELEASE, (key) => {
-    socket.to(roomId).emit(RECEIVE_OPPONENT_KEY_RELEASE, key);
-  });
-
-  socket.on(SEND_START, () => {
-    socket.to(roomId).emit(RECEIVE_START);
-  });
-
-  socket.on(SEND_READY, () => {
-    socket.to(roomId).emit(RECEIVE_READY);
-  });
-
-  socket.on(SEND_BATTLES, (score, combo, word) => {
-    socket.to(roomId).emit(RECEIVE_BATTLES, score, combo, word);
-  });
-
-  io.of("/").emit(FROM_BATTLEROOM, convertedRooms);
-
-  socket.on("disconnect", () => {
-    const { uid, roomId } = socket.handshake.query;
-    room.users.delete(uid);
+    const room = battleRooms[roomId] || { users: new Set() };
+    room.users.add(uid);
     battleRooms[roomId] = room;
 
-    usersInRoom[roomId] = usersInRoom[roomId].filter((u) => u.uid !== uid);
+    if (room.users.size > 2) {
+      socket.emit(ROOM_FULL);
+      socket.leave(roomId);
+      return;
+    }
 
-    socket.to(roomId).emit(USER_LEFT, uid);
-    socket.to(roomId).emit(RECEIVE_BATTLES, null);
+    const convertedRooms = Object.keys(battleRooms).reduce((acc, key) => {
+      acc[key] = { users: Array.from(battleRooms[key].users) };
+      return acc;
+    }, {});
+
+    if (!usersInRoom[roomId]) {
+      usersInRoom[roomId] = [];
+    }
+    usersInRoom[roomId].push(user);
+
+    socket.on(SEND_CONNECT, () => {
+      io.emit(LOBBY_ROOMS, convertedRooms);
+    });
+
+    socket.on(SEND_USER, () => {
+      socket.to(roomId).emit(RECEIVE_USER, usersInRoom[roomId]);
+    });
+
+    socket.on(DELETE_ROOM, () => {
+      socket.to(roomId).emit(RECEIVE_DELETE);
+    });
+
+    socket.on(OPPONENT_KEY_PRESS, (key) => {
+      socket.to(roomId).emit(RECEIVE_OPPONENT_KEY_PRESS, key);
+    });
+
+    socket.on(OPPONENT_KEY_RELEASE, (key) => {
+      socket.to(roomId).emit(RECEIVE_OPPONENT_KEY_RELEASE, key);
+    });
+
+    socket.on(SEND_START, () => {
+      socket.to(roomId).emit(RECEIVE_START);
+    });
+
+    socket.on(SEND_READY, () => {
+      socket.to(roomId).emit(RECEIVE_READY);
+    });
+
+    socket.on(SEND_BATTLES, (score, combo, word) => {
+      socket.to(roomId).emit(RECEIVE_BATTLES, score, combo, word);
+    });
+
+    io.of("/").emit(FROM_BATTLEROOM, convertedRooms);
+
+    socket.on("disconnect", () => {
+      const { uid, roomId } = socket.handshake.query;
+      room.users.delete(uid);
+      battleRooms[roomId] = room;
+
+      usersInRoom[roomId] = usersInRoom[roomId].filter((u) => u.uid !== uid);
+
+      socket.to(roomId).emit(USER_LEFT, uid);
+      socket.to(roomId).emit(RECEIVE_BATTLES, null);
+    });
   });
-});
 
-results.on("connection", (socket) => {
-  const { photoURL, displayName, uid, resultId } = socket.handshake.query;
-  socket.join(resultId);
+  results.on("connection", (socket) => {
+    const { photoURL, displayName, uid, resultId } = socket.handshake.query;
+    socket.join(resultId);
 
-  const user = { photoURL, displayName, uid };
+    const user = { photoURL, displayName, uid };
 
-  socket.on(SEND_RESULTS, (comboResults, totalScore) => {
-    socket.to(resultId).emit(RECEIVE_RESULTS, comboResults, totalScore, user);
+    socket.on(SEND_RESULTS, (comboResults, totalScore) => {
+      socket.to(resultId).emit(RECEIVE_RESULTS, comboResults, totalScore, user);
+    });
+
+    socket.on("disconnect", () => {
+      delete usersInRoom[resultId];
+    });
   });
-
-  socket.on("disconnect", () => {
-    delete usersInRoom[resultId];
-  });
-});
+};
